8f7d5473cdd529b37b2af0bf0c2921fb482d672d
Tim Lee <krunch522@gmail.com>
2021-03-11 17:43:48 +0800

    adjust some content

--- Assignment3/forData.js
+++ Assignment3/forData.js
@@ -1,51 +1,36 @@
 const photoNum=8;
+let photoCount=0;
+let data;
 window.onload=function(){
     let req=new XMLHttpRequest();
     req.open("get", "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json");
     req.onload=function(){
-        let data=JSON.parse(req.responseText);
-        getData(data.result.results);
+        data = JSON.parse(req.responseText).result.results;
+        dealData(data);
     };
     req.send();
 };
 
-function getData(spotsData){
-    //const imgMatches=document.querySelectorAll("img");
-    //console.log(imgMatches);
-    //let imgUrl=document.createElement("img");
-    //imgUrl.src=spotsData[0].file;
-    //document.body.main.appendChild(imUrl);
-    for(i=0;i<photoNum;i++){
+let btn=document.getElementById("btn");
+btn.addEventListener("click", function(){
+    photoCount+=photoNum;
+    dealData(data);    
+});
+
+function dealData(spotsData){
+    for(i=photoCount;i<photoCount+photoNum;i++){
+        let container=document.createElement("div");
+        container.classList.add("container");
+        document.querySelector(".photosContainer").appendChild(container);
         const firstUrl="http://"+spotsData[i].file.split('http://')[1];
-        const photoTitle=spotsData[i].stitle;
+        const spotName=spotsData[i].stitle;
         let imgUrl=document.createElement("img");
         let photoName=document.createElement("div");
+        photoName.classList.add("context");
         imgUrl.src=firstUrl;
-        photoName.textContent=photoTitle;
-        document.querySelectorAll('.container')[i].appendChild(imgUrl);
-        document.querySelectorAll('.container')[i].appendChild(photoName);
-    }    
-};
-
-/*
-function getData(){
-    if(spotsData===undefined){
-        let req=new XMLHttpRequest();
-        req.open("get", "https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json");
-        req.onload=function(){
-            spotsData=JSON.parse(req.responseText);
-            console.log(spotsData);
-            console.log("222222222222");
-        };
-        req.onerror=function(){
-            console.log("Connection error");
-        };
-        req.send();
+        photoName.textContent=spotName;
+        document.querySelectorAll(".container")[i].appendChild(imgUrl);
+        document.querySelectorAll(".container")[i].appendChild(photoName);
     };
-    const matchs=document.querySelectorAll("img");
-    console.log(matchs);
-    console.log(spotsData);
 };
-*/
-
 
